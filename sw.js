a1c0a768ceae583935e87fa230de4f316a47ff13
yonems67-jpg <yonems67@gmail.com>
2026-03-30 13:07:01 +0800

    Update service worker cache and file paths

--- sw.js
+++ sw.js
@@ -1,20 +1,26 @@
-const CACHE = "fakao-v1";
-const FILES = ["/", "/index.html", "/manifest.json", "/icon.png"];
+const CACHE = “fakao-v2”;
+const FILES = [
+“/fakao/”,
+“/fakao/index.html”,
+“/fakao/manifest.json”,
+“/fakao/icon.png”,
+“/fakao/sw.js”
+];
 
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
-  self.skipWaiting();
+self.addEventListener(“install”, e => {
+e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+self.skipWaiting();
 });
 
-self.addEventListener("activate", e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
+self.addEventListener(“activate”, e => {
+e.waitUntil(caches.keys().then(keys =>
+Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+));
+self.clients.claim();
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match("/index.html")))
-  );
+self.addEventListener(“fetch”, e => {
+e.respondWith(
+caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match(”/fakao/index.html”)))
+);
 });
